76ac5b0773bbe797946ef46a1d0855294292e295
Tony Bowden <tony@tmtm.com>
2022-02-15 15:14:51 +0200

    compare Mato Grosso do Sul governors

---
+++ sources/governor-mato-grosso-do-sul-ptwiki/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'mandato'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[ordinal name image dates].freeze
+    end
+
+    def raw_combo_date
+      super.gsub('até', ' – ').gsub(/\(.*?\)/, '')
+    end
+
+    def empty?
+      (tds[3].text == tds[4].text) || (startDate < '1985-01-01')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
