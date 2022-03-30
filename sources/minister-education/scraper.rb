8ebfff2db91ea23d3735acf4656ac16c0b7d9c6c
Tony Bowden <tony@tmtm.com>
2022-03-30 09:05:48 +0300

    compare ptwiki Education Ministers

---
+++ sources/minister-education/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Foto'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[number name img ministry start end].freeze
+    end
+
+    def raw_end
+      super.downcase.gsub(' de de ', ' de ')
+    end
+
+    def empty?
+      itemLabel == 'vago'
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
