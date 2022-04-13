e950c6ae5f9ab09c7cb9440cb1e22f873b3fcb3e
Tony Bowden <tony@tmtm.com>
2022-04-13 09:41:10 +0300

    compare eswiki Justice Ministers

---
+++ sources/minister-justice/scraper.rb
@@ -0,0 +1,29 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Órgão'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[number name ministry start end].freeze
+    end
+
+    #TODO: push up
+    def raw_end
+      super.gsub('atualmente', '')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
